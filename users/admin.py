b6859afa2cf8b61ccf4ae3369ece2730be489565
civil777 <dinnerisconclude@naver.com>
2020-06-18 17:30:29 +0900

    3.6

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
 
+    """ Custom User Admin"""
 
-# Register your models here.
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {"fields": ("프로필_사진", "성별", "기타사항", "language", "통화", "할인_혜택_회원", "생일",)},
+        ),
+    )
--- users/models.py
+++ users/models.py
@@ -4,4 +4,42 @@ from django.db import models
 
 class User(AbstractUser):
 
-    pass
+    """ User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "남자"),
+        (GENDER_FEMALE, "여자"),
+        (GENDER_OTHER, "기타"),
+    )
+
+    LANGUAGE_ENGLISH = "english"
+    LANGUAGE_KOREAN = "korean"
+    LANGUAGE_CHINESE = "chinese"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "한국어"),
+        (LANGUAGE_CHINESE, "Chinese"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD $"),
+        (CURRENCY_KRW, "KRW 원"),
+    )
+
+    기타사항 = models.TextField(default="", blank=True)
+    프로필_사진 = models.ImageField(null=True, blank=True)
+    성별 = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
+    생일 = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=7, null=True, blank=True
+    )
+    통화 = models.CharField(choices=CURRENCY_CHOICES, max_length=7, null=True, blank=True)
+    할인_혜택_회원 = models.BooleanField(default=False)
